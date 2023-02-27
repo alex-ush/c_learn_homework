3750ddd1e4cea996b7081e8cde88af7f742d95e0
alex-ush <mastak@bk.ru>
2023-02-27 19:32:35 +0300

    done

--- Homework_4/Exercise_3/Program.cs
+++ Homework_4/Exercise_3/Program.cs
@@ -2,14 +2,6 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-// int Prompt (string message)
-// {
-//     System.Console.Write(message);
-//     string ReadInput = System.Console.ReadLine();
-//     int result = int.Parse(ReadInput);
-//     return result;
-// }
-
 int[] GenerateArray (int Length, int minValue, int maxValue)
 {
     int[] array = new int[Length];
@@ -31,11 +23,6 @@ void PrintArray(int[] array)
     System.Console.Write($"{array[array.Length - 1]}");
     System.Console.WriteLine("]");
 }
-
-// int length = Prompt ("Длина массива: ");
-// int min = Prompt ("Начальное значение диапазона случайных чисел: ");
-// int max = Prompt ("Конечное значение диапазона случайных чисел: ");
-// int[] array = GenerateArray(length, min, max);
 
 int[] array = GenerateArray(8, 1, 10);
 PrintArray(array);
