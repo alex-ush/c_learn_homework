8e9b1d1276d0497a930385b1d62ed91c5d24ecaf
alex-ush <mastak@bk.ru>
2023-02-27 19:30:08 +0300

    done

---
+++ Homework_4/Exercise_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Pow(int A, int B)
+{
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+result *= A;
+}
+return result;
+}
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+int PowResult = Pow(A, B);
+
+Console.WriteLine(A + " в степени " + B + " будет " + PowResult);
---
+++ Homework_4/Exercise_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNum(int Number)
+{
+    int Result = 0;
+    while (Number > 0)
+    {
+        Result += Number % 10;
+        Number = Number / 10;
+    }
+    return Result;
+}
+
+Console.Write("Введите ваше число, а я посчитаю сумму всех его чисел: ");
+int Number = int.Parse(Console.ReadLine());
+
+int SumNumResult = SumNum(Number);
+
+Console.WriteLine("Сумма всех чисел равна " + SumNumResult);
